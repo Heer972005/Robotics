43d160ced95cdf2d539d93bc51db863d4427cf5a
Heer972005 <mehtaheer.972@gmail.com>
2026-01-01 17:02:18 +0530

    fixed

---
+++ FrozenLake/play.py
@@ -0,0 +1,35 @@
+import gymnasium as gym
+import time
+from q_learning import greedy_policy
+import numpy as np
+import pickle
+
+# Load Q-table
+with open("qtable.pkl", "rb") as f:
+    Qtable = pickle.load(f)
+
+# Create environment with human rendering
+env = gym.make(
+    "FrozenLake-v1",
+    map_name="4x4",
+    is_slippery=False,
+    render_mode="human"
+)
+
+state, _ = env.reset()
+done = False
+
+print("🎮 Agent is playing FrozenLake...")
+
+while not done:
+    action = greedy_policy(Qtable, state)
+    state, reward, terminated, truncated, _ = env.step(action)
+    done = terminated or truncated
+    time.sleep(0.6)  # slow down for visibility
+
+if reward == 1:
+    print("🏆 Agent reached the goal!")
+else:
+    print("❌ Agent failed!")
+
+env.close()
---
+++ FrozenLake/train.py
@@ -0,0 +1,29 @@
+import gymnasium as gym
+from q_learning import train_q_learning
+import pickle
+
+env = gym.make(
+    "FrozenLake-v1",
+    map_name="4x4",
+    is_slippery=False,
+    render_mode=None,
+)
+
+Qtable = train_q_learning(
+    env=env,
+    n_training_episodes=10000,
+    learning_rate=0.7,
+    gamma=0.95,
+    max_steps=99,
+    max_epsilon=1.0,
+    min_epsilon=0.05,
+    decay_rate=0.0005,
+)
+
+print("Training finished!")
+print(Qtable)
+
+with open("qtable.pkl", "wb") as f:
+    pickle.dump(Qtable, f)
+
+print("💾 Q-table saved as qtable.pkl")
